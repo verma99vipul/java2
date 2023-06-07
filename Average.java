c7c5fc1e8215348b580d6233e81b73a158b109a2
verma99vipul <v99vipul@gmail.com>
2023-06-07 14:59:17 +0530

    java program added

---
+++ Average.java
@@ -0,0 +1,11 @@
+public  class Average{
+	public static void main(String[] args){
+		float  num1= 32;
+	float  num2= 100;
+		float num3= 200;
+		float avrg= (num1+num2+num3)/100;
+
+		System.out.println(avrg);
+
+	}
+}
